5ad64b14269aeb396e47fc4acff14422aee574b9
hellolintao <hellolintao@126.com>
2018-12-15 17:55:17 +0800

    'update'

---
+++ 实战/常用组件开发之便签页组件/app.vue
@@ -0,0 +1,63 @@
+<template>
+	<div>
+		<h2>自定义组件之标签页</h2>
+		<v-tabs v-model="activeKey">
+			<v-pane label="标签一" name="1">
+				标签一的内容
+			</v-pane>
+			<v-pane label="标签二" name="2">
+				标签二的内容
+			</v-pane>
+			<v-pane label="标签三" name="3">
+				标签三的内容
+			</v-pane>
+		</v-tabs>
+	</div>
+</template>
+<script>
+	import vTabs from "./tabs.vue";
+	import vPane from "./pane.vue";
+	export default {
+		components: {
+			vTabs,
+			vPane
+		},
+		data: function () {
+			return {
+				activeKey: "1"
+			}
+		}
+	}
+</script>
+<style>
+	[v-cloak] {
+		display: none;
+	}
+
+	.tabs {
+		font-size: 14px;
+		color: #657180;
+	}
+	.tabs-bar:after {
+		content: " ";
+		display: block;
+		width: 100%;
+		height: 1px;
+		background: #f2f2f2;
+		margin-top: -1px;
+	}
+	.tabs-tab {
+		display: inline-block;
+		padding: 4px 16px;
+		margin-right: 6px;
+		background: #ffffff;
+		border: 1px solid #eeeeee;
+		cursor: pointer;
+		position: relative;
+	}
+	.tabs-tab-active {
+		color: red;
+		border-top: 1px solid blue;
+		border-bottom: 1px solid #ffff;
+	}
+</style>
---
+++ 实战/常用组件开发之便签页组件/pane.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="pane" v-show="show">
+		<slot></slot>
+	</div>
+</template>
+<script>
+	export default {
+		props: {
+			name: {
+				type: String
+			},
+			label: {
+				type: String,
+				default: ""
+			}
+		},
+		data: function() {
+			return {
+				show: true
+			}
+		},
+		methods: {
+			updateNav: function() {
+				console.log(this.$parent.updateNav);
+				this.$parent.updateNav();
+				// this.$parent.updateNav();
+			}
+		},
+		watch: {
+			label: function() {
+				this.updateNav();
+			}
+		},
+		mounted: function() {
+			this.updateNav();
+		}
+	}
+</script>
+<style>
+
+</style>
