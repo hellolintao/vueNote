38c4436851ce2e3fdb1362b549bde479a671ea63
hellolintao <hellolintao@126.com>
2018-12-17 21:22:50 +0800

    update

---
+++ 实战/自定义指令之可以从外部关闭的下拉菜单/clickoutside.js
@@ -0,0 +1,23 @@
+Vue.directive('clickoutside', {
+	bind: function(el, binding, vnode) {
+		console.log('指令运行');
+		// binding是一个对象，里面包含了很多的属性
+		function documentHandler(e) {
+			// 如果是自身元素，则返回
+			if(el.contains(e.target)) {
+				return false;
+			}
+			// 如果指令中有绑定的字符串，那么
+			if(binding.expression) {
+				// 运行指令中绑定的值，binding.value是指定绑定值！
+				binding.value(e);
+			}
+		}
+		el.__vueClickOutside__ = documentHandler;
+		document.addEventListener('click', documentHandler);
+	},
+	unbind: function(el, binding) {
+		documentHandler.removeEventListener('click', el.__vueClickOutside__);
+		delete el.__vueClickOutside__;
+	}
+});
