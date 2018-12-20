a9f349bd1f83a5ffb72a219f8c063d0bec92434a
hellolintao <hellolintao@126.com>
2018-12-20 09:49:19 +0800

    update

--- 实战/使用Render函数开发可排序的表格组件/table.js
+++ 实战/使用Render函数开发可排序的表格组件/table.js
@@ -1,5 +1,7 @@
+/* 声明组件 */
 Vue.component("vTable", {
 	props: {
+		/* 外部传递进来的需要2个数据，默认是空的 */
 		columns: {
 			type: Array,
 			default:function() {
@@ -15,12 +17,14 @@ Vue.component("vTable", {
 	},
 	data: function() {
 		return {
+			/* 为了不破坏原有数据，将数据保存咋这里 */
 			currentColumns: [],
 			currentData: []
 		}
 	},
 	methods: {
 		makeColumns: function() {
+			/* 处理表头数据--加入排序类型和索引 */
 			this.currentColumns = this.columns.map(function(col, index) {
 				col._sortType = "normal";
 				col._index = index;
@@ -28,19 +32,23 @@ Vue.component("vTable", {
 			});
 		},
 		makeData: function() {
+			/* 处理表体数据，加入索引 */
 			this.currentData = this.data.map(function(row, index) {
 				row._index = index;
 				return row;
 			});
 		},
 		handleSortByAsc: function(index) {
+			/* 升序排列算法 */
 			console.log('handleSortByAsc');
 		},
 		handleSortByDesc: function(index) {
+			/* 降序排列算法 */
 			console.log("降序");
 		}
 	},
 	mounted: function() {
+		/* 加载完成后，首先处理原始数据 */
 		this.makeColumns();
 		this.makeData();
 	},
